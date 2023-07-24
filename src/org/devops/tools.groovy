d51f38d183e597d01c2db234bef4c0e315b70b44
tangzengwei <tangzengwei@x-y-t.cn>
2023-07-24 22:14:42 +0800

    add printColor

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,5 +1,22 @@
 package org.devops
 
+// 打印内容
+
 def PrintMes(content) {
   println(content)
 }
+
+// 格式化输出
+
+def PrintColor(value, color) {
+  colors = [
+    'red': "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<\033[0m",
+    'blue': "\033[47;34m  ${value} \033[0m",
+    'green': "\033[40;32m >>>>>>>>>>${value}<<<<<<\033[0m"
+  ]
+  ansiColor('xterm') {
+    println(colors[color])
+  }
+}
+
+
